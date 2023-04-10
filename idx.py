57a43224cd928a83366784dd355d6627f9049bad
Jason Chadwick <jchadwick100@gmail.com>
2023-04-10 14:36:21 -0500

    update to prefer numpy

--- idx.py
+++ idx.py
@@ -2,7 +2,13 @@ import numpy as np
 from functools import reduce
 
 def idx_from_bits(bits, nt):
-    nbits = len(nt)
+    bits = np.array(bits)
+    nt = np.array(nt)
+    if len(bits.shape) == 0:
+        bits = bits[None]
+    if len(nt.shape) == 0:
+        nt = nt[None]
+    nbits = nt.shape[0]
     acc = 0
     for b in range(nbits):
         bit_value = reduce(lambda x,y:x*y, nt[b+1:], 1)
@@ -10,10 +16,13 @@ def idx_from_bits(bits, nt):
     return acc
 
 def bits_from_idx(idx, nt):
-    nbits = len(nt)
+    nt = np.array(nt)
+    if len(nt.shape) == 0:
+        nt = nt[None]
+    nbits = nt.shape[0]
     acc = []
     for b in range(nbits):
         bit_value = reduce(lambda x,y:x*y, nt[b+1:], 1)
         acc.append(idx // bit_value)
         idx %= bit_value
-    return acc
+    return np.array(acc)
