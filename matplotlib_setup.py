852b70edb9615eb1b33985e721a416ceb1157143
Jason Chadwick <jchadwick100@gmail.com>
2023-11-22 13:43:15 -0500

    add marker cycle

--- matplotlib_setup.py
+++ matplotlib_setup.py
@@ -10,6 +10,8 @@ nature_colors = ['#0072B2', '#CC79A7', '#009E73', '#E69F00', '#56B4E9', '#D55E00
 colors = nature_colors
 colors_light = ['#6BAFD4', '#E7B6D1', '#6BCAB0', '#F2CE7C', '#A8D9F4', '#ECB182', '#F5F0A1']
 
+markers = ['o', 'X', 's', 'P', 'd', '^', 'v']
+
 opensans_path = '/Users/jchad/Library/Fonts/OpenSans-VariableFont_wdth,wght.ttf'
 
 def set_font(path: str = opensans_path, size: Optional[int] = 12) -> None:
@@ -35,7 +37,7 @@ def set_colors(colors: list[str] = colors) -> None:
     Args:
         colors: list of hexadecimal color codes, e.g. "#0072B2".
     """
-    plt.rcParams['axes.prop_cycle'] = mpl.cycler(color=colors) 
+    plt.rcParams['axes.prop_cycle'] = mpl.cycler(color=colors, marker=markers) 
 
 def setup_default() -> None:
     """Call set_font and set_colors with default options.
