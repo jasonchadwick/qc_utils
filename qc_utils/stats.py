c6a890d713c1f3e0896dff606910b9548ad55184
Jason Chadwick <jchadwick100@gmail.com>
2024-02-08 15:23:30 -0600

    add sinter's confidence interval functions

--- qc_utils/stats.py
+++ qc_utils/stats.py
@@ -1,6 +1,7 @@
 import numpy as np
 from numpy.typing import NDArray
 import scipy
+import math
 
 def likelihood_ratio_CI(count: float, ntrials: int, confidence: float = 0.95) -> tuple[float, float]:
     """Compute confidence interval on an estimated proportion via Likelihood Ratio Test.
@@ -130,3 +131,102 @@ def get_most_probable_bitstrings(biases: NDArray[np.float_], n_bitstrings, proba
         probabilities.append(chosen_prob)
     
     return chosen_bitstrings, np.array(probabilities)
+
+def log_factorial(n: int) -> float:
+    """Approximates $\ln(n!)$; the natural logarithm of a factorial. Copied from
+    sinter's `log_factorial` function in `sinter/_probability_util.py`.
+
+    Args:
+        n: The input to the factorial.
+
+    Returns:
+        Evaluates $ln(n!)$ using `math.lgamma(n+1)`.
+    """
+    return math.lgamma(n + 1)
+
+def log_binomial(*, p: float | np.ndarray, n: int, hits: int) -> np.ndarray:
+    """Approximates the natural log of a binomial distribution's probability.
+    Copied from sinter's `log_binomial` function in
+    `sinter/_probability_util.py`.
+
+    This method evaluates $\ln(P(hits = B(n, p)))$, with all computations done
+    in log space to ensure intermediate values can be represented as floating
+    point numbers without underflowing to 0 or overflowing to infinity. This
+    method can be broadcast over multiple hypothesis probabilities by giving a
+    numpy array for `p` instead of a single float.
+
+    Args:
+        p: The hypotehsis probability. The independent probability of a hit
+            occurring for each sample. This can also be an array of
+            probabilities, in which case the function is broadcast over the
+            array.
+        n: The number of samples that were taken.
+        hits: The number of hits that were observed amongst the samples that
+            were taken.
+
+    Returns:
+        $\ln(P(hits = B(n, p)))$
+    """
+    # Clamp probabilities into the valid [0, 1] range (in case float error put them outside it).
+    p_clipped = np.clip(p, 0, 1)
+
+    result = np.zeros(shape=p_clipped.shape, dtype=np.float32)
+    misses = n - hits
+
+    # Handle p=0 and p=1 cases separately, to avoid arithmetic warnings.
+    if hits:
+        result[p_clipped == 0] = -np.inf
+    if misses:
+        result[p_clipped == 1] = -np.inf
+
+    # Multiply p**hits and (1-p)**misses onto the total, in log space.
+    result[p_clipped != 0] += np.log(p_clipped[p_clipped != 0]) * hits
+    result[p_clipped != 1] += np.log1p(-p_clipped[p_clipped != 1]) * misses
+
+    # Multiply (n choose hits) onto the total, in log space.
+    log_n_choose_hits = log_factorial(n) - log_factorial(misses) - log_factorial(hits)
+    result += log_n_choose_hits
+
+    return result
+
+def fit_binomial(
+        *,
+        num_shots: int,
+        num_hits: int,
+        max_likelihood_factor: float) -> tuple[float, float]:
+    """Determine hypothesis probabilities compatible with the given hit ratio.
+    Modified from sinter's `fit_binomial` function in 
+    `sinter/_probability_util.py`.
+
+    The result includes the best fit (the max likelihood hypothis) as well as
+    the smallest and largest probabilities whose likelihood is within the given
+    factor of the maximum likelihood hypothesis.
+
+    Args:
+        num_shots: The number of samples that were taken.
+        num_hits: The number of hits that were seen in the samples.
+        max_likelihood_factor: The maximum Bayes factor between the low/high
+            hypotheses and the best hypothesis (the max likelihood hypothesis).
+            This value should be larger than 1 (as opposed to between 0 and 1).
+
+    Returns:
+        A `sinter.Fit` with the low, best, and high hypothesis probabilities.
+    """
+    if max_likelihood_factor < 1:
+        raise ValueError(f'max_likelihood_factor={max_likelihood_factor} < 1')
+    if num_shots == 0:
+        return (0.0, 1.0)
+    log_max_likelihood = log_binomial(p=num_hits / num_shots, n=num_shots, hits=num_hits)
+    target_log_likelihood = log_max_likelihood - math.log(max_likelihood_factor)
+    acc = 100
+    low = scipy.optimize.minimize(
+        func=lambda exp_err: log_binomial(p=exp_err / (acc * num_shots), n=num_shots, hits=num_hits) - target_log_likelihood,
+        x0=num_hits*acc,
+        bounds=(0, num_hits * acc),
+    ) / acc
+    high = scipy.optimize.minimize(
+        func=lambda exp_err: -log_binomial(p=exp_err / (acc * num_shots), n=num_shots, hits=num_hits) + target_log_likelihood,
+        x0=num_hits*acc,
+        bounds=(num_hits * acc, num_shots * acc),
+    ) / acc
+    return (low / num_shots, high / num_shots)
