44b0e667479cb6dfd2fb09b89177ad48ef93ca11
Jason Chadwick <jchadwick100@gmail.com>
2024-02-08 16:17:22 -0600

    fix fit_binomial

--- qc_utils/stats.py
+++ qc_utils/stats.py
@@ -220,13 +220,13 @@ def fit_binomial(
     target_log_likelihood = log_max_likelihood - math.log(max_likelihood_factor)
     acc = 100
     low = scipy.optimize.minimize(
-        fun=lambda exp_err: log_binomial(p=exp_err / (acc * num_shots), n=num_shots, hits=num_hits) - target_log_likelihood,
-        x0=num_hits*acc,
-        bounds=(0, num_hits * acc),
+        fun=lambda exp_err: log_binomial(p=exp_err[0] / (acc * num_shots), n=num_shots, hits=num_hits) - target_log_likelihood,
+        x0=[num_hits*acc],
+        bounds=[(0, num_hits * acc)],
     ) / acc
     high = scipy.optimize.minimize(
-        fun=lambda exp_err: -log_binomial(p=exp_err / (acc * num_shots), n=num_shots, hits=num_hits) + target_log_likelihood,
-        x0=num_hits*acc,
-        bounds=(num_hits * acc, num_shots * acc),
+        fun=lambda exp_err: -log_binomial(p=exp_err[0] / (acc * num_shots), n=num_shots, hits=num_hits) + target_log_likelihood,
+        x0=[num_hits*acc],
+        bounds=[(num_hits * acc, num_shots * acc)],
     ) / acc
     return (low / num_shots, high / num_shots)
